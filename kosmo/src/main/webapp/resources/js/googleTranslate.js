8fc50c8bdda44db10ec3c49a4016190a496d5729
kosmo43team3 <kosmo43team3@gmail.com>
2019-01-29 21:00:44 +0900

    재영 뉴스 게시판 신설! 뚜둥!

--- kosmo/src/main/webapp/resources/js/googleTranslate.js
+++ kosmo/src/main/webapp/resources/js/googleTranslate.js
@@ -1,7 +1,7 @@
  $('document').ready(function () {
             $('#google_translate_element').on("click", function () {
 
-                // Change font family and color
+            	//구글 검색 창 색상 바꾸기1
                 $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div") //, .goog-te-menu2 *
                 .css({
                     'color': '#70c745',
@@ -9,19 +9,19 @@
                     'font-family': '"Open Sans",Helvetica,Arial,sans-serif'
                 });
 
-                // Change hover effects  #e3e3ff = white
+                //구글 검색 창 색상 바꾸기2
                 $("iframe").contents().find(".goog-te-menu2-item div").hover(function () {
                     $(this).css('background-color', '#70c745').find('span.text').css('color', '#fff');
                 }, function () {
                     $(this).css('background-color', '#fff').find('span.text').css('color', '#70c745');
                 });
 
-                // Change Google's default blue border
+                //구글 검색 창 색상 바꾸기3
                 $("iframe").contents().find('.goog-te-menu2').css('border', '1px solid #70c745');
 
                 $("iframe").contents().find('.goog-te-menu2').css('background-color', '');
 
-                // Change the iframe's box shadow
+                //구글 검색 창 색상 바꾸기4
                 $(".goog-te-menu-frame").css({
                     '-moz-box-shadow': '0 3px 8px 2px #666666',
                     '-webkit-box-shadow': '0 3px 8px 2px #666',
